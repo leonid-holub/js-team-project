a54f700e02dfb509d45e9ba6755012be673aaa68
Leonid Holub <rsleonid@gmail.com>
2022-10-08 16:38:30 +0300

    Додає список країн до вибору.

--- src/js/datalist_countries.js
+++ src/js/datalist_countries.js
@@ -1,15 +1,15 @@
 import { suppCountries } from './all_supp_countries';
 
-// const queryCountry = document.querySelector('.header__form #countries');
-// const defaultCountryDataList = function () {
-//   const countriesList = suppCountries.getKeysAndValues();
-//   countriesList.forEach((elem, index) => {
-//     queryCountry.insertAdjacentHTML(
-//       'beforeend',
-//       `<option value="${elem[1]}">${elem[0]}</option>`
-//     );
-//   });
-// };
-// defaultCountryDataList();
+const queryCountry = document.querySelector('.header__form #countries');
+const defaultCountryDataList = function () {
+  const countriesList = suppCountries.getKeysAndValues();
+  countriesList.forEach((elem, index) => {
+    queryCountry.insertAdjacentHTML(
+      'beforeend',
+      `<option value="${elem[1]}">${elem[0]}</option>`
+    );
+  });
+};
+defaultCountryDataList();
 
-// console.log(queryCountry);
+console.log(queryCountry);
