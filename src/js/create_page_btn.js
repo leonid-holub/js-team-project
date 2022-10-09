5a2d9e21abe111e76ca7a1a5ec0354e74572f8c4
Buzanov Kostya <buzanov.kostya@gmail.com>
2022-10-09 14:06:27 +0300

    Pagination complete

--- src/js/create_page_btn.js
+++ src/js/create_page_btn.js
@@ -1,20 +1,28 @@
 export const paginationBtns = document.querySelector('[data-pagination]');
 export default function createPageBtn(value) {
   const quantityBtn = [];
+  let pageBtns = [];
   for (let i = 1; i <= value; i += 1) {
     quantityBtn.push(i);
   }
-  const pageBtns = quantityBtn.map(btn => {
-    if (btn > 5) {
-      return;
-    } else {
-      return `<li><button class="pagination-btn " href="#" type="button" btn-pagination>${btn}</button></li>`;
-    }
-  });
-  pageBtns.push(
-    '...',
-    `<li><button class="pagination-btn " href="#" type="button" btn-pagination>${value}</button></li>`
-  );
+  if (value < 5) {
+    pageBtns = quantityBtn.map(
+      btn =>
+        `<li><button class="pagination-btn " href="#" type="button" btn-pagination>${btn}</button></li>`
+    );
+  } else if (value > 5) {
+    pageBtns = quantityBtn.map(btn => {
+      if (btn > 5) {
+        return;
+      } else {
+        return `<li><button class="pagination-btn " href="#" type="button" btn-pagination>${btn}</button></li>`;
+      }
+    });
+    pageBtns.push(
+      '...',
+      `<li><button class="pagination-btn " href="#" type="button" btn-pagination>${value}</button></li>`
+    );
+  }
 
   return (paginationBtns.innerHTML = pageBtns.join(''));
 }
--- src/js/pagination.js
+++ src/js/pagination.js
@@ -2,9 +2,10 @@ import createPageBtn from '../js/create_page_btn';
 import finalPageBtn from '../js/final_page_btn';
 import updatePageBtn from '../js/update_page_btn';
 import endPageBtn from '../js/end_page_btn';
-
+import paginationFetch from './pagination_fetch';
 const paginationMenu = document.querySelector('[data-pagination]');
 let pageBtns = document.querySelectorAll('[btn-pagination]');
+
 export let totalPages = 0;
 export let btnValue = 0;
 
@@ -20,12 +21,17 @@ export function getTotal(total) {
 function onBtnClick(e) {
   e.preventDefault();
   btnValue = Number(e.target.textContent);
+  paginationFetch(btnValue);
   if (!e.target.hasAttribute('btn-pagination')) {
     return;
   }
   removeIsActive();
-  chooseCreateBtn(e.target);
-  addIsActive();
+  if (pageBtns.length <= 4) {
+    return addIsActive();
+  } else {
+    chooseCreateBtn(e.target);
+    return addIsActive();
+  }
 }
 
 function chooseCreateBtn(elem) {
