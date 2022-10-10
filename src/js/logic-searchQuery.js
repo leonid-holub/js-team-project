53ad559430ec9707571dcc390d94463445292442
Andriy-Martsynyak <a.marcynak@ukr.net>
2022-10-10 10:44:28 +0300

    bagfix

--- src/js/logic-searchQuery.js
+++ src/js/logic-searchQuery.js
@@ -10,7 +10,9 @@ const searchField = document.querySelector('.header__input');
 const onSearchFildChange = function (e) {
   const eventName = e.target.value.trim();
   fetchFromAPI.config.params.keyword = eventName;
-
+  if (eventName.length < 2) {
+    return;
+  }
   fetchFromAPI.baseFetch().then(response => {
     const eventList = response.page.totalElements;
     if (eventList === 0) {
