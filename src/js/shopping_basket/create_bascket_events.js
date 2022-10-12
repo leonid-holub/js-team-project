6dae7966d754d27bc32246c68a5e11578337d0da
Buzanov Kostya <buzanov.kostya@gmail.com>
2022-10-12 15:42:58 +0300

    complete

--- src/js/shopping_basket/create_bascket_events.js
+++ src/js/shopping_basket/create_bascket_events.js
@@ -1,19 +1,19 @@
-const basketEvents = document.querySelector(
-  '.modal-shopping-basket__container-events'
-);
-let event;
+const favoritesEvents = document.querySelector('.modal-favorites__info');
 
-export function createBasketEvents() {
+export  function createBasketEvents() {
   const objEvent = JSON.parse(localStorage.getItem('Events'));
   const newEvent = objEvent
     .map(
-      obj => `<ul class="modal-shopping-basket__info-events">
-          <li class="modal-shopping-basket__info-events-elem">${obj.name}</li>
-          <li class="modal-shopping-basket__info-events-elem">${obj.country}</li>
-          <li class="modal-shopping-basket__info-events-elem" data-basket-price>${obj.price}</li>
-          <li class="modal-shopping-basket__info-events-elem">1</li>
-        </ul>`
+      (obj, index) => `<ul class="modal-favorites__info-events">
+        <li class="modal-favorites__info-events-elem"><img class="modal-favorites__info-events-img" src="${obj.imgEvent}" alt="${obj.name}" width="60" ></img></li>
+        <li class="modal-favorites__info-events-elem">${obj.name}</li>
+        <li class="modal-favorites__info-events-elem">${obj.country}</li>
+        <li class="modal-favorites__info-events-elem">${obj.city}</li>
+        <li class="modal-favorites__info-events-elem" data-basket-price>${obj.dataEvents}</li>
+        <li class="modal-favorites__info-events-elem"><a class="modal-favorites__info-link" href="${obj.urlBuyTicket}">BUY TICKET</a></li>
+        <li class="modal-favorites__info-events-elem"><button class="modal-favorites__info-link" data="${index}" data-favorites-delete>DELETE</button></li>
+      </ul>`
     )
     .join('');
-  basketEvents.innerHTML = newEvent;
+  favoritesEvents.innerHTML = newEvent;
 }
